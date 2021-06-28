c563d86da78d6af4b0d3664335450e8df20dd878
mustafa fathi <mustafa.fathi7@gmail.com>
2021-06-28 02:22:49 +0200

    download dependencies and setup database and express app

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+// read .env file
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// Setup database
+const connectionKey = process.env.CONNECTION_URL,
+  port = process.env.PORT || 5000;
+
+mongoose
+  .connect(connectionKey, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port: http://localhost:${port}`));
+  })
+  .catch((e) => console.log(e.message));
+
+  mongoose.set('useFindAndModify', false)
